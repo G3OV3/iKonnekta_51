6f15fb04093d09de0d3e4fc48430763baf13f2af
themagicdude231 <themagicdude231@gmail.com>
2026-05-05 20:22:33 +0800

    Vosotros/Staff

---
+++ Controllers/VStaffController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iKonnekta_51.Controllers
+{
+    public class VStaffController : Controller
+    {
+        // GET: VStaff
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult VDashboardViewPage()
+        {
+            return View();
+        }
+        public ActionResult VManageRequestViewPage()
+        {
+            return View();
+        }
+        public ActionResult VRequestHistory_RecordsViewPage()
+        {
+            return View();
+        }
+        public ActionResult VRegisteredResidentsViewPage()
+        {
+            return View();
+        }
+        public ActionResult VArchivesViewPage()
+        {
+            return View();
+        }
+        public ActionResult VNotificationViewPage()
+        {
+            return View();
+        }
+        public ActionResult VListofResidentsViewPage()
+        {
+            return View();
+        }
+        public ActionResult VAddResidentViewPage()
+        {
+            return View();
+        }
+    }
+}
