8dd1efb4ac26c4737f0b87156047f5573683c021
G3OV3 <geogomboc@gmail.com>
2026-05-07 01:46:37 +0800

    latest local copy

--- Scripts/my-scripts/Controller.js
+++ Scripts/my-scripts/Controller.js
@@ -20,6 +20,54 @@
             case 'forgotPassword':
                 window.location.href = "/iKonnekta_51/ForgotPasswordPage"
                 break;
+
+            // Resident Pages
+            case 'residentDashboard':
+                window.location.href = "/Resident/DashboardPage"
+                break;
+            case 'submitRequest':
+                window.location.href = "/Resident/SubmitRequestPage"
+                break;
+            case 'trackRequest':
+                window.location.href = "/Resident/TrackRequestPage"
+                break;
+            case 'requestHistorypage':
+                window.location.href = "/Resident/HistoryPage_Resident"
+                break;
+            case 'notification':
+                window.location.href = "/Resident/NotificationPage"
+                break;
+            case 'residentProfile':
+                window.location.href = "/Resident/ResidentProfilePage"
+                break;
+
+            // Staff Pages
+            case 'staffDashboard':
+                window.location.href = "/VStaff/VDashboardViewPage"
+                break;
+            case 'AddResident':
+                window.location.href = "/VStaff/VAddResidentViewPage"
+                break;
+            case 'Archive':
+                window.location.href = "/VStaff/VArchivesViewPage"
+                break;
+            case 'ListOfResidents':
+                window.location.href = "/VStaff/VListofResidentsViewPage"
+                break;
+            case 'ManageRequests':
+                window.location.href = "/VStaff/VManageRequestViewPage"
+                break;
+            case 'staffNotification':
+                window.location.href = "/VStaff/VNotificationViewPage"
+                break;
+            case 'AccsOfResidents':
+                window.location.href = "/VStaff/VRegisteredResidentsViewPage"
+                break;
+            case 'requestHistoryRecords':
+                window.location.href = "/VStaff/VRequestHistory_RecordsViewPage"
+                break;
+            case 'editResidentInfo':
+                window.location.href = "/VStaff/VViewEditResidentInfoViewPage"
         }
     }
     // OTP Part
@@ -95,6 +143,18 @@
         $scope.sidebarOpen = false
     }
 
+    //Prevent Letters for phone textbox
+    $scope.$watch('phone', function (newVal, oldVal) {
+        if (!newVal) return;
+
+        var clean = newVal.replace(/[^0-9]/g, '');
+
+        if (clean !== newVal) {
+            $scope.phone = clean;
+        }
+    });
+
+
     // Resident Part
 
     //TrackRequests
@@ -139,11 +199,10 @@
     //Notification 
     $scope.currentTab = 'all';
 
-  
+
     $scope.allNotifications = [
         {
-            id: 1, icon: '📄', iconBg: 'bg-blue-50',
-            badge: 'Processing', badgeCss: 'bg-blue-100 text-blue-700',
+            id: 1, icon: '📄',
             title: 'Document request approved',
             sub: 'Your Certification of proof of Residency is processing.',
             time: 'Just now', read: false,
@@ -174,6 +233,7 @@
     $scope.markAllRead = function () {
         $scope.allNotifications.forEach(function (n) { n.read = true; });
     };
+
 
 
 });
